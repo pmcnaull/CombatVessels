531e8203c125c5bf367243e71b00349c847ce055
Patrick McNaull <pmcnaull@gmail.com>
2014-11-04 20:42:36 -0500

    Initial solution commit

---
+++ CombatVessels/CombatVessels/CombatVesselsMainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombatVessels
+{
+    public partial class CombatVesselsMainForm : Form
+    {
+        public CombatVesselsMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
